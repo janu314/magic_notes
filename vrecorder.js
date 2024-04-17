475ea1306e9f2b379d34cdd338997d457980721a
Janu Subramanian <janu314@gmail.com>
2024-04-17 17:17:28 -0400

    V1.0 added from desktop

---
+++ vrecorder.js
@@ -0,0 +1,145 @@
+// collect DOMs
+const display = document.querySelector('.display')
+const controllerWrapper = document.querySelector('.controllers')
+
+const State = ['Initial', 'Record', 'Download']
+let stateIndex = 0
+let mediaRecorder, chunks = [], audioURL = ''
+
+// mediaRecorder setup for audio
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+    console.log('mediaDevices supported..')
+
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    }).then(stream => {
+        mediaRecorder = new MediaRecorder(stream)
+
+        mediaRecorder.ondataavailable = (e) => {
+            chunks.push(e.data)
+        }
+
+        mediaRecorder.onstop = () => {
+            const blob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'})
+            chunks = []
+            audioURL = window.URL.createObjectURL(blob)
+            document.querySelector('audio').src = audioURL
+
+        }
+    }).catch(error => {
+        console.log('Following error has occured : ',error)
+    })
+}else{
+    stateIndex = ''
+    application(stateIndex)
+}
+
+const clearDisplay = () => {
+    display.textContent = ''
+}
+
+const clearControls = () => {
+    controllerWrapper.textContent = ''
+}
+
+const record = () => {
+    stateIndex = 1
+    mediaRecorder.start()
+    application(stateIndex)
+}
+
+const stopRecording = () => {
+    stateIndex = 2
+    mediaRecorder.stop()
+    application(stateIndex);
+    
+}
+
+
+const saveUserInfo = (filename) => {
+    const name = prompt("Please enter your name:");
+    const email = prompt("Please enter your email:");
+    const className = prompt("Please enter the name of your class:");
+
+    const timestamp = filename.match(/(\d+)/)[0]; //extracxt
+    const userInfoFilename = `userinfo_${timestamp}_${email}`;
+    
+    const userInfo = `Name: ${name}\nEmail: ${email}\nClass: ${className}\nAudio File: ${filename}`;
+    const userInfoBlob = new Blob([userInfo], { type: 'text/plain' });
+
+    const userInfoLink = document.createElement('a');
+    userInfoLink.href = URL.createObjectURL(userInfoBlob);
+    userInfoLink.setAttribute('download', `${userInfoFilename}.txt`);
+    userInfoLink.click();
+}
+
+
+const downloadAudio = () => {
+    const timestamp = new Date().getTime(); // Get current timestamp
+    const filename = 'audio_' + timestamp + '.ogg'; // Construct filename using timestamp
+    saveUserInfo(filename);
+    const downloadLink = document.createElement('a');
+    downloadLink.href = audioURL;
+    downloadLink.setAttribute('download', filename); // Set filename
+    downloadLink.setAttribute('target', '_blank'); // Open in new tab
+    downloadLink.click();
+}
+
+const addButton = (id, funString, text) => {
+    const btn = document.createElement('button')
+    btn.id = id
+    btn.setAttribute('onclick', funString)
+    btn.textContent = text
+    controllerWrapper.append(btn)
+}
+
+const addMessage = (text) => {
+    const msg = document.createElement('p')
+    msg.textContent = text
+    display.append(msg)
+}
+
+const addAudio = () => {
+    const audio = document.createElement('audio')
+    audio.controls = true
+    audio.src = audioURL
+    display.append(audio)
+}
+
+const application = (index) => {
+    switch (State[index]) {
+        case 'Initial':
+            clearDisplay()
+            clearControls()
+
+            addButton('record', 'record()', 'Start Recording')
+            break;
+
+        case 'Record':
+            clearDisplay()
+            clearControls()
+
+            addMessage('Recording...')
+            addButton('stop', 'stopRecording()', 'Stop Recording')
+            break
+
+        case 'Download':
+            clearControls()
+            clearDisplay()
+
+            addAudio()
+            addButton('download', 'downloadAudio()', 'Download Audio')
+            addButton('record', 'record()', 'Record Again')
+            break
+
+        default:
+            clearControls()
+            clearDisplay()
+
+            addMessage('Your browser does not support mediaDevices')
+            break;
+    }
+
+}
+
+application(stateIndex)
